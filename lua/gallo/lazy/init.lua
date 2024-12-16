0059293152c91c4d4b7df937e76c146d58aee50e
Caio C Gallo <caio.gallo@gmail.com>
2024-12-16 13:27:43 -0300

    added colors

--- lua/gallo/lazy/init.lua
+++ lua/gallo/lazy/init.lua
@@ -1,10 +1,3 @@
 return {
-	"folke/neodev.nvim",
-	"folke/which-key.nvim",
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
 
-	-- lsp-zero	
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
 }
