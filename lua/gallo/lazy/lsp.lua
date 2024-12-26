92e126db9dc739dcff89d3964dadf63f48d5ebdc
Caio C Gallo <caio.gallo@gmail.com>
2024-12-26 11:30:05 -0300

    install telescope and treesitter

--- lua/gallo/lazy/lsp.lua
+++ lua/gallo/lazy/lsp.lua
@@ -1,13 +1,17 @@
 return {
+    -- Collection of common configuration for the Nvim LSP client
     "neovim/nvim-lspconfig",
     dependencies = {
         "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+	-- cmp LSP completion
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
+	-- cmp Path completion
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
+	-- Autocompletetion framework
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
