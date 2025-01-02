ee93061d737fe6035bf43f9faf08ebd1bebe5a2b
Caio C Gallo <caio.gallo@gmail.com>
2025-01-02 10:52:21 -0300

    configure telescope

--- lua/gallo/lazy/colorscheme.lua
+++ lua/gallo/lazy/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function() 
-			vim.cmd("colorscheme rose-pine")
-		end
-	}
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function() 
+            vim.cmd("colorscheme rose-pine")
+        end
+    }
 }
