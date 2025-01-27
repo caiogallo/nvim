64b6f1f464b77682e6a742c709d01cd613ec53d6
Caio C Gallo <caio.gallo@gmail.com>
2025-01-27 12:01:59 -0300

    trying to config rust import

--- lua/gallo/lazy/lsp.lua
+++ lua/gallo/lazy/lsp.lua
@@ -1,16 +1,95 @@
---
--- local function attach(opts) {
---
---}
-
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
         "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
     },
     config = function()
+        local cmp = require('cmp')
+        local cmp_lsp = require('cmp_nvim_lsp')
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
         require("mason").setup()
-        require("mason-lspconfig").setup()
+        require("mason-lspconfig").setup {
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer"
+            },
+            handlers = {
+                function (server_name)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+
+                ["rust_analyzer"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.rust_analyzer.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            ["rust_analyzer"] = {
+                                assist = {
+                                    importGranularity = "module",
+                                    importPrefix = "by_self",
+                                },
+                            }
+                        },
+                    })
+                end,
+            }
+        }
+
+        local cmp_select = { behaivor = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    --require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    vim.fn["vsnip#anonymous"](args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'buffer' },
+                { name = 'path' },
+            }
+--            sources = cmp.config.sources({
+--                { name = 'nvim_lsp' },
+--                { name = 'luasnip' }, -- For luasnip users.
+--            }, {
+--                { name = 'buffer' },
+--            })
+        })
     end
 }
