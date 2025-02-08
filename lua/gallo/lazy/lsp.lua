ec77926d9f3d4a3b3a5181d1971c14a2bd6af390
Caio C Gallo <caio.gallo@gmail.com>
2025-02-08 18:22:39 -0300

    fix missing configs

---
+++ lua/gallo/lazy/lsp.lua
@@ -0,0 +1,106 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "simrat39/rust-tools.nvim",
+    },
+
+    config = function() 
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "rust_analyzer", "lua_ls",
+            },
+        })
+
+        local lspconfig = require('lspconfig')
+        local lsp_defaults = lspconfig.util.default_config
+
+        lsp_defaults.capabilities = vim.tbl_deep_extend(
+            'force',
+            lsp_defaults.capabilities,
+            require('cmp_nvim_lsp').default_capabilities()
+        )
+
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua= {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        library = {
+                            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                            [vim.fn.stdpath "config" .. "/lua"] = true,
+                        },
+                    },
+                },
+            },
+        })
+
+        -- setup rust-tools
+        local rt = require("rust-tools")
+        rt.setup({
+            server = {
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                on_attach = function(_, bufnr)
+                    -- Hover actions
+                    vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                    -- Code action groups
+                    vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+
+                    -- vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+                end
+            },
+            tools = {
+                hover_actions = {
+                    auto_focus = true,
+                },
+                -- using neovim 0.10 inlay hints
+                inlay_hints = { 
+                    auto = false
+                }
+            },
+        })
+        
+        -- on-typing autocomplete
+        local cmp = require('cmp')
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+
+   end
+}
